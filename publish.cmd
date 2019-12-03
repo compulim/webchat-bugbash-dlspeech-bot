3710dc52f5cf61678342b8707e764362def5ea73
William Wong <compulim@hotmail.com>
2019-12-03 11:47:56 -0800

    Initial commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild EchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=webchat-bugbash-dlspeech-Web-Deploy.pubxml -p:Password=u5Eu5EnKTDHfxrbjhFErSXqsEsiwfkR6i1Xuzszpdq1ezJf7w2Rs2gRvK8Cf
+
